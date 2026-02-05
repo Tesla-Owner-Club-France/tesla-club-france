42bba6c68478f1c49481971a1b2a607a91b9e18c
LUTTER Régis <regis.lutter@gmail.com>
2026-02-05 11:17:38 +0100

    feat(redirect): redirect home to partners

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/partenaires",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
